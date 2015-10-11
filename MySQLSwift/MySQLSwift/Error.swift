9550ec4935336729d50acd2b28289d1ac2545bb8
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-10-11 12:44:43 -0500

    working on query and getting results

---
+++ MySQLSwift/MySQLSwift/Error.swift
@@ -0,0 +1,16 @@
+//
+//  Error.swift
+//  MySQLSwift
+//
+//  Created by Alsey Coleman Miller on 10/11/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+public extension MySQL {
+    
+    public enum Error: ErrorType {
+        
+        /// ```mysql_store_result()``` should have returned data
+        case BadResult
+    }
+}
--- MySQLSwift/MySQLSwift/MySQL.swift
+++ MySQLSwift/MySQLSwift/MySQL.swift
@@ -7,6 +7,7 @@
 //
 
 import mysql
+import SwiftFoundation
 
 public final class MySQL {
     
@@ -54,7 +55,7 @@ public final class MySQL {
     /// Attempts to establish a connection to a MySQL database engine.
     public func connect(host: String, user: String, password: String, database: String? = nil, port: UInt32 = 0, options: [ClientOption] = []) throws {
         
-        let clientFlags: UInt32 = 0
+        let clientFlags: UInt = 0
         
         if let database = database {
             
@@ -68,10 +69,87 @@ public final class MySQL {
         }
     }
     
+    // MARK: Database Operations
+    
     public func selectDatabase(database: String) throws {
         
         guard mysql_select_db(internalPointer, database) == 0
             else { throw ClientError(rawValue: mysql_errno(internalPointer))! }
     }
+    
+    public func createDatabase(database: String) throws {
+        
+        guard mysql_create_db(internalPointer, database) == 0
+            else { throw ClientError(rawValue: mysql_errno(internalPointer))! }
+    }
+    
+    public func deleteDatabase(database: String) throws {
+        
+        guard mysql_drop_db(internalPointer, database) == 0
+            else { throw ClientError(rawValue: mysql_errno(internalPointer))! }
+    }
+    
+    // MARK: Query
+    
+    public func executeQuery(query: String) throws {
+        
+        guard mysql_exec_sql(internalPointer, query) == 0
+            else { throw ClientError(rawValue: mysql_errno(internalPointer))! }
+    }
+    
+    public func getResults() throws -> [[Data]]? {
+        
+        let mysqlResult = mysql_store_result(internalPointer)
+        
+        guard mysqlResult != nil else {
+            
+            guard mysql_field_count(internalPointer) == 0
+                else { throw Error.BadResult }
+            
+            return nil
+        }
+        
+        defer { mysql_free_result(mysqlResult) }
+        
+        var rowResults = [[Data]]()
+        
+        var row: MYSQL_ROW
+        
+        repeat {
+            
+            row = mysql_fetch_row(mysqlResult)
+            
+            let numberOfFields = mysql_num_fields(mysqlResult)
+            
+            let lastIndex = Int(numberOfFields - 1)
+            
+            var fieldsData = [Data]()
+            
+            for i in 0...lastIndex {
+                
+                let value = row[i]
+                
+                var data = Data()
+                
+                // get data from char array
+                
+                
+                fieldsData.append(data)
+            }
+            
+            rowResults.append(fieldsData)
+            
+        } while row != nil
+        
+        return rowResults
+    }
 }
+
+// MARK: - Function Definitions
+
+@asmname("mysql_create_db") func mysql_create_db(mysql: UnsafeMutablePointer<MYSQL>, _ database: UnsafePointer<CChar>) -> Int32
+
+@asmname("mysql_drop_db") func mysql_drop_db(mysql: UnsafeMutablePointer<MYSQL>, _ database: UnsafePointer<CChar>) -> Int32
+
+@asmname("mysql_exec_sql") func mysql_exec_sql(mysql: UnsafeMutablePointer<MYSQL>, _ SQL: UnsafePointer<CChar>) -> Int32
 
