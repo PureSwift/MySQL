1cb467891e7b61d26d8345895a5fa81fcc98891d
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-10 01:13:05 -0500

    updating package info

--- Package.swift
+++ Package.swift
@@ -3,6 +3,14 @@ import PackageDescription
 let package = Package(
     name: "MySQL",
     dependencies: [
+        .Package(url: "https://github.com/PureSwift/CMySQL.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/SwiftFoundation.git", majorVersion: 1),
+    ],
+    targets: [
+        Target(
+            name: "UnitTests",
+            dependencies: [.Target(name: "MySQL")]),
+        Target(
+            name: "MySQL")
     ]
 )
