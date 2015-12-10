ea698ae7ba2bc8be22405e866bd54c84ec8be1c7
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-10 01:26:44 -0500

    removed setup / tear down functions

--- Sources/UnitTests/MySQLTests.swift
+++ Sources/UnitTests/MySQLTests.swift
@@ -18,16 +18,6 @@ class MySQLTests: XCTestCase {
         ]
     }
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testNewDB() {
         
         let connection = MySQL.Connection()
