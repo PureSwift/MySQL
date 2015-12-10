b63f7cca5e53021c97a04ce1e5c781fe9babdb92
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-10 01:46:00 -0500

    working on Unit test for Linux

--- Sources/UnitTests/main.swift
+++ Sources/UnitTests/main.swift
@@ -7,9 +7,80 @@
 //
 
 import SwiftFoundation
+import MySQL
 import XCTest
     
 #if os(Linux)
     import Glibc
-    XCTMain([MySQLTests()])
+#endif
+
+let connection = MySQL.Connection()
+
+do { try connection.connect(hostname, user: user, password: password, databaseName: nil, port: port, socket: socket) }
+    
+catch { XCTFail("Could not connect: \(error)") }
+
+let secondsSinceReferenceDate = Int(Date().timeIntervalSinceReferenceDate)
+
+let databaseName = "TestDatabase\(secondsSinceReferenceDate)"
+
+print("Creating test database: " + databaseName)
+
+// create DB, insert data, and then fetch
+do {
+    
+    try connection.createDatabase(databaseName)
+    
+    try connection.selectDatabase(databaseName)
+    
+    try connection.query("CREATE TABLE family (Name char(20),Room char(8),Phone char(24))")
+    
+    try connection.query("INSERT INTO family VALUES ('Gomez Adams', 'master', '1-555-1212')")
+    
+    guard let result = try connection.query("SELECT * FROM family") where result.rowCount == 1
+        else { XCTFail("No results returned") }
+    
+    var stringResults = [[String]]()
+    
+    for (_, row) in EnumerateGenerator(result.rows) {
+        
+        let stringValues = row.values.map { (data) in String(UTF8Data: data)! }
+        
+        stringResults.append(stringValues)
+    }
+    
+    // print
+    print("Results: ")
+    for (index, row) in stringResults.enumerate() { print("\(index) = \(row)") }
+    
+    #if os(OSX)
+        XCTAssert(stringResults == [["Gomez Adams", "master", "1-555-1212"]], "Results must match inserted data")
+    #else
+        XCTAssert(stringResults.count == 1)
+        XCTAssert(stringResults[0][0] == "Gomez Adams")
+        XCTAssert(stringResults[0][1] == "master")
+        XCTAssert(stringResults[0][2] == "1-555-1212")
+    #endif
+    
+}
+
+catch { XCTFail("Error: \(error))") }
+
+exit(0)
+
+#if os(Linux)
+    
+    @noreturn
+    func XCTFail(string: String) { print(string); exit(1) }
+
+    @noreturn
+    func XCTAssert(@autoclosure expression: () -> Bool, _ message: String = "Test failed") {
+    
+    if !expression() {
+        
+        print(message)
+        exit(1)
+    }
+}
+
 #endif
