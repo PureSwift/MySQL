5e3b2afed46cd16b73a86b1cf71af5f0fe2910b6
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-13 14:20:23 -0500

    removed extra ","

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "MySQL",
     dependencies: [
         .Package(url: "https://github.com/PureSwift/CMySQL.git", majorVersion: 1),
-        .Package(url: "https://github.com/PureSwift/SwiftFoundation.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/SwiftFoundation.git", majorVersion: 1)
     ],
     targets: [
         Target(
